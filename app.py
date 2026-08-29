46ba61e485ded87423d1bf29614465f77267018a
MARTINSERGIO3519 <u22213582@utp.edu.pe>
2026-08-29 18:08:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ def resta(a, b):
     """Calcula la resta de dos números."""
     return a - b
 
-def obtener_mensaje_bienvenida(nombre="Usuario"):
+def obtener_meensaje_bienvenida(nombre="Usuario"):
     """Retorna un mensaje de bienvenida personalizado."""
     return f"¡Hola, {nombre}! Bienvenido a la aplicación."
 
-if __name__ == "__main__":
+if __name__ ==____ "__main__":
     print(obtener_mensaje_bienvenida("DevOps Team"))
     
-    resultado_suma = suma(10, 5)
+  "  resultado_suma = suma(10, 5)"
     resultado_resta = resta(10, 5)
     
     print(f"Resultado Suma (10 + 5): {resultado_suma}")
